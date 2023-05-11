fa290855f34bfc92037413edadb9a2eeb7093cc4
vincepogz <vincepogz@mailbox.org>
2023-05-11 15:23:08 -0400

    random object generator complete

--- backend/employee/employee.js
+++ backend/employee/employee.js
@@ -1,4 +1,5 @@
 const SKILLS = require('./employeeSkills.json')
+const EMPLOYEE_NAMES = require('./employeeNames.json')
 employeeCount = 1; 
 
 const DEFAULT_PROPERTIES = {
@@ -6,10 +7,38 @@ const DEFAULT_PROPERTIES = {
     employeeId: 0,
     employeeTasked: false,
     employeeName: 'unnamed',
+    employeeGender: 'unset',
     employeeType: 'unset',
     employeeSalary: 0,
     employeeSkills: [],
     
+}
+
+const getEmployeeName = function (employeeGender) {
+    const surnames = EMPLOYEE_NAMES.surname;
+    const surname = surnames[
+        Math.floor(Math.random() * surnames.length)
+    ].name.value;
+
+    if (employeeGender == 'male'){
+        const employeeFirstName = EMPLOYEE_NAMES.male[
+            Math.floor(Math.random() * EMPLOYEE_NAMES.male.length)
+        ].name.value
+
+        const employeeName = employeeFirstName.concat(' ', surname)
+
+        return employeeName
+
+    }else {
+        const employeeFirstName = EMPLOYEE_NAMES.female[
+            Math.floor(Math.random() * EMPLOYEE_NAMES.female.length)
+        ].name.value
+
+        const employeeName = employeeFirstName.concat(' ', surname)
+
+        return employeeName
+    }
+
 }
 
 const getSalaryRange = function (employeeType) {
@@ -68,10 +97,11 @@ const getEmployeeSkills = function (employeeType) {
 };
 
 class Employee {
-    constructor(employeeType, employeeName) {
+    constructor(employeeType, employeeGender) {
         this.employeeId = employeeCount++ || DEFAULT_PROPERTIES.employeeId;
         this.employeeTasked = DEFAULT_PROPERTIES.employeeTasked;
-        this.employeeName = employeeName || DEFAULT_PROPERTIES.employeeName;
+        this.employeeName = getEmployeeName(employeeGender) || DEFAULT_PROPERTIES.employeeName;
+        this.employeeGender = employeeGender || DEFAULT_PROPERTIES.employeeGender;
         this.employeeType = employeeType || DEFAULT_PROPERTIES.employeeType;
         this.employeeSalary = getEmployeeSalary(employeeType) || DEFAULT_PROPERTIES.employeeSalary;
         this.employeeSkills = getEmployeeSkills(employeeType) || DEFAULT_PROPERTIES.employeeSkills;
